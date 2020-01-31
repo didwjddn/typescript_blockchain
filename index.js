5d4cceaf20ec8c767deaf62f84d3bf380dec1069
didwjddn <didwjddn@konkuk.ac.kr>
2020-01-31 11:23:06 +0900

    tuto

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name},you are ${age}, you are a ${gender}`);
+};
+sayHi("yang", 44444, "male");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,7 @@
+
+const sayHi= (name: String, age: Number, gender:String) => {
+    console.log(`hello ${name},you are ${age}, you are a ${gender}`);
+};
+sayHi("yang",44444,"male")
+
+export {};
